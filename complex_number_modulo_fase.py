4b0671716abd37cf5fc8f736b06fb9340ae9546c
Ing. Di Filippo <davidedifilippo@gmail.com>
2023-01-10 14:25:57 +0100

    Update complex_number_modulo_fase.py

--- complex_number_modulo_fase.py
+++ complex_number_modulo_fase.py
@@ -1,5 +1,5 @@
 import numpy as np
-from complex_plane import complex_plane2
+
 
 c1_M = input("Inserire il modulo del numero complesso: ")
 c1_phi = input("Inserire la fase del numero complesso in gradi: ")
@@ -13,19 +13,17 @@ c1_phi_rad = (c1_phi/180.0) * np.pi
 c1 = c1_M * (np.cos(c1_phi_rad) + np.sin(c1_phi_rad)*1j)
 
 
-print("Numero inserito: ", np.round(c1, 2))
+print("\nNumero inserito: ", np.round(c1, 2))
 
-print("Coordinate cartesiane: ")
+print("\nCoordinate cartesiane: ")
 print("x=", np.round(c1.real,2))
 print("y=", np.round(c1.imag,2))
 
-print("x=", c1.imag)
 
-print("Coordinate polari: ")
+
+print("\nCoordinate polari:")
 print("Modulo=", c1_M)
 print("Angolo=", c1_phi)
-
-
 
 
 
